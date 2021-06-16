737004d31a61d50cdc0529f23208d2a90af4129f
Alexander Cyon <alex.cyon@gmail.com>
2021-06-16 11:25:15 +0200

    add empty transaction.c file to make testing work.

---
+++ src/transaction/transaction.c
@@ -0,0 +1,5 @@
+#include "transaction.h"
+
+// Used in CMakeLists.txt for unit test so we can write `add_library(transaction
+// ../src/transaction/transaction.c)`. This is kind of hacky and surely we can do better, I just
+// lack the CMake knowledge to improve this. Please feel free to fix!
--- src/transaction/transaction.h
+++ src/transaction/transaction.h
@@ -4,7 +4,7 @@
 #include "../types/uint256.h"
 
 typedef struct {
-    uint256_t tx_fee;  /// The tee of this transaction, measured in XRD.
+    uint256_t tx_fee;  /// The fee of this transaction, measured in XRD.
     uint256_t total_xrd_amount_incl_fee;
 
     bool have_asserted_no_mint_or_burn;
