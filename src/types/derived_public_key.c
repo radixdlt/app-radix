56d03744212f7361e4cc33c5f8341ea5df8718ef
Alexander Cyon <alex.cyon@gmail.com>
2021-06-17 17:08:55 +0200

    fix build issues for unit test

---
+++ src/types/derived_public_key.c
@@ -0,0 +1,5 @@
+#include "derived_public_key.h"
+
+// Used in CMakeLists.txt for unit test so we can write `add_library(derived_public_key
+// ../src/types/derived_public_key.c)`. This is kind of hacky and surely we can do better, I just
+// lack the CMake knowledge to improve this. Please feel free to fix!
