6ac05b042ac46c3ca478a3aee2d3f16514ce1bc5
Alexander Cyon <alex.cyon@gmail.com>
2021-06-19 09:03:51 +0200

    reintroduce debug_print file

---
+++ unit-tests/util/debug_print.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "../src/transaction/transaction_parser.h"  // parse_and_process_instruction_outcome_t
+
+void dbg_print_parse_process_instruction_outcome(parse_and_process_instruction_outcome_t *outcome);
